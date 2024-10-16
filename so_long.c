a0aee5a55a96fcdc5dd828b05f721c7859714875
Bryan <89350478+sabahbryan@users.noreply.github.com>
2024-10-16 22:57:09 +0800

    Add files via upload

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: bryaloo <bryaloo@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 21:33:57 by bryaloo           #+#    #+#             */
-/*   Updated: 2024/10/15 21:34:44 by bryaloo          ###   ########.fr       */
+/*   Updated: 2024/10/16 16:37:05 by bryaloo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
 
 	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
