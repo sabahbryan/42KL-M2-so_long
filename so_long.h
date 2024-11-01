c752490ce4522b65f3a8665ae2b6883665b690ce
Bryan <89350478+sabahbryan@users.noreply.github.com>
2024-11-01 23:20:00 +0800

    space_to_tab
    update header and Makefile, then change spaces to tabs

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: bryaloo <bryaloo@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 22:32:21 by bryaloo           #+#    #+#             */
-/*   Updated: 2024/10/28 21:38:57 by bryaloo          ###   ########.fr       */
+/*   Updated: 2024/11/01 23:18:23 by bryaloo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,73 @@
 
 # include "../libft/libft.h"
 // errno.h, math.h
-# include <stdio.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <mlx.h>
+#include <mlx.h>           // MiniLibX library for graphics
+#include <stdlib.h>        // Standard library functions (malloc, free, etc.)
+#include <fcntl.h>         // File control options (open)
+#include <unistd.h>        // UNIX standard functions (write, read, close)
+
+// Constants for window dimensions
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+// Constants for key codes (modify based on your system's codes)
+#define KEY_W 13           // Up
+#define KEY_A 0            // Left
+#define KEY_S 1            // Down
+#define KEY_D 2            // Right
+#define KEY_ESC 53         // ESC key to exit
+
+// Game structure to hold all necessary data
+typedef struct s_game {
+	void	*mlx;           // MiniLibX pointer
+	void	*win;           // Window pointer
+	char	**map;          // 2D map array loaded from the .ber file
+	int		map_width;      // Width of the map
+	int		map_height;     // Height of the map
+	int		player_x;       // Player x-coordinate
+	int		player_y;       // Player y-coordinate
+	int		total_collectibles; // Total collectibles in the map
+	int		player_collected;   // Number of collected items by the player
+	int		player_at_exit;     // Flag to check if player reached the exit
+} t_game;
+
+//***FUNCTION PROTOTYPES***
+
+// map_loader.c
+char	**load_map(char *filename);
+int		validate_map(char **map);
+int		check_rectangular(char **map);
+int		check_boundary_walls(char **map);
+int		has_required_elements(char **map, int width, int height);
+int		check_path(char **map, int width, int height);
+int		map_height(char **map);
+
+// main.c
+int		init_game(t_game *game, char *map_file);
+int		close_game(t_game *game);
+
+// input_handler.c
+int		handle_keypress(int keycode, t_game *game);
+void	move_player(t_game *game, int dx, int dy);
+
+// game_loop.c
+void	game_loop(t_game *game);
+int		update_game(t_game *game);
+
+// render.c
+void	render_map(t_game *game);
+void	render_tile(t_game *game, int x, int y);
+void	draw_wall(t_game *game, int x, int y);
+void	draw_collectible(t_game *game, int x, int y);
+void	draw_exit(t_game *game, int x, int y);
+void	draw_player(t_game *game, int x, int y);
+void	draw_floor(t_game *game, int x, int y);
+
+// validation.c
+void	flood_fill(t_game *game, int x, int y);
+void	*print_error(char *message);
+
+#endif
 
 /*struct
 	player
@@ -32,5 +95,3 @@
 /*
 	src functions here
  */
-
-#endif
